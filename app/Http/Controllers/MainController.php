94d35a5e1f5188426404016754891d7f6ebdbbd4
David Guerra Da Silva Cravo <david.guerradsc@gmail.com>
2021-01-14 23:27:16 +0100

    update instructors

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Category;
 use App\Course;
+use App\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function home() {
+
+        $instructors = User::has('courses')->get();
+        
+
        $category = Category::where('id', 2)->firstOrFail();
        
-       return view('main.home');
+       return view('main.home', [
+           'instructors' => $instructors
+       ]);
     }
+
+
 }
