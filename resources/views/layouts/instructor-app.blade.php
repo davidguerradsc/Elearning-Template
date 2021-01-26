562494f078d9d99c4fc7ac847039928439e735d8
David Guerra Da Silva Cravo <david.guerradsc@gmail.com>
2021-01-26 11:49:12 +0100

    Revert "Merge branch 'Develop'"
    This reverts commit 503dbe623ce1e64d6f88370bf18dc08ccc235799, reversing
    changes made to 620c237faeffe5644bb41fd59a3e9008434499f8.

--- resources/views/layouts/instructor-app.blade.php
+++ resources/views/layouts/instructor-app.blade.php
@@ -7,14 +7,14 @@
 
     <!-- Bootstrap CSS -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="{{ secure_asset('css/elegant-icons.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/magnific-popup.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/slicknav.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/elegant-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/magnific-popup.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/slicknav.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <script src="https://kit.fontawesome.com/907683f3ba.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Learning</title>
@@ -46,14 +46,14 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!-- Popper js -->
-    <script src="{{ secure_asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
     <!-- Bootstrap js -->
-    <script src="{{ secure_asset('js/jquery.countdown.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.countdown.min.js') }}"></script>
     <!-- All Plugins js -->
-    <script src="{{ secure_asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
     <!-- Active js -->
-    <script src="{{ secure_asset('js/jquery.slicknav.js') }}"></script>
-    <script src="{{ secure_asset('js/main.js') }}"></script> 
-    <script src="{{ secure_asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.slicknav.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script> 
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
   </body>
 </html>
